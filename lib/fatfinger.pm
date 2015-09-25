3f521b1e8d3c440f7814e570e3241b58f88f7581
Olaf Alders <olaf@wundersolutions.com>
2015-09-24 21:31:53 -0400

    Search filesystem if %INC comes up empty.

--- lib/fatfinger.pm
+++ lib/fatfinger.pm
@@ -1,15 +1,20 @@
 use strict;
 use warnings;
+use feature qw( say );
 package fatfinger;
 
+use File::Spec;
+use Path::Iterator::Rule;
 use Text::Levenshtein qw(distance);
 
 sub import {
     push @INC, sub {
         shift;
         my $name = shift;
+        return if $name eq 'prefork.pm';
 
         my $module = _maybe_find_module_in_INC($name);
+        $module ||= _maybe_find_module_on_disk($name);
         return unless $module;
 
         my $msg = <<"EOF";
@@ -31,6 +36,50 @@ sub _maybe_find_module_in_INC {
         if ( distance( $file, $module ) <= 2 ) {
             $file =~ s{/}{::}g;
             return $file if $file;
+        }
+    }
+}
+
+sub _maybe_find_module_on_disk {
+    my $module = shift;
+
+    my $rule = Path::Iterator::Rule->new;
+    $rule->perl_module;
+
+    # iterator interface
+    my @dirs = grep { !ref $_ } @INC;
+
+    foreach my $dir (@dirs) {
+        my $modules = 0;
+        my $next    = $rule->iter($dir);
+        while ( defined( my $file = $next->() ) ) {
+            my $orig_file = $file;
+
+            # remove top level path
+            $file =~ s{^$dir}{}g;
+            my @parts = reverse( File::Spec->splitdir($file) );
+
+            # work from the bottom up until we hit illegal chars
+            my @name;
+            my $count = 0;
+            for my $part (@parts) {
+                if ( $count > 0 && ( $part !~ m{\w} || $part =~ m{[\.\-]} ) )
+                {
+                    last;
+                }
+                push @name, $part;
+                ++$count;
+            }
+            $file = join '::', reverse @name;
+
+            if ( $ENV{FF_DEBUG} ) {
+                say "f: $file m: $module " . distance( $file, $module );
+            }
+
+            if ( distance( lc($file), lc($module) ) <= 2 ) {
+                $file =~ s{\.pm\z}{};
+                return $file;
+            }
         }
     }
 }
--- t/fatfinger.t
+++ t/fatfinger.t
@@ -4,17 +4,25 @@ use warnings;
 use Test::Fatal;
 use Test::More;
 
-use fatfinger;
-
 use lib 't/lib';
+use fatfinger;
 
 like(
     exception { require FF::Truncated; },
-    qr{Did you really mean strict\.pm},
-    'recommends strict.pm',
+    qr{Did you really mean strict},
+    'recommends strict',
 );
 
-#use FF::Truncated;
-#require FF::Truncated;
+like(
+    exception { require FooBarX; },
+    qr{Did you really mean FooBar},
+    'recommends FooBar',
+);
+
+like(
+    exception { require XFooBarX; },
+    qr{Did you really mean FooBar},
+    'recommends FooBar',
+);
 
 done_testing();
